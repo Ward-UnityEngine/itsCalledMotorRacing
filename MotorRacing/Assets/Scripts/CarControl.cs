9d2f960f04b55b20a4645357cc9535dfd596b9eb
TimRens <tim1.rens@telenet.be>
2023-04-01 13:16:28 +0200

    tried tutorial from docs useful weblinks but doesnt work yet

---
+++ MotorRacing/Assets/Scripts/CarControl.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+
+    public List<WheelElements> wheelData;
+
+    public float maxTorque;
+    public float maxSteerAngle = 30;
+
+    private void FixedUpdate()
+    {
+        float speed = Input.GetAxis("Vertical") * maxTorque;
+        float steer = Input.GetAxis("Horizontal") * maxSteerAngle;
+
+        foreach (WheelElements wheel in wheelData)
+        {
+            if (wheel.shouldSteer)
+            {
+                wheel.leftWheel.steerAngle = steer;
+                wheel.rightWheel.steerAngle = steer;
+            }
+
+            if (wheel.addWheelTorque)
+            {
+                wheel.leftWheel.motorTorque = speed;
+                wheel.rightWheel.motorTorque = speed;
+            }
+
+            doTires(wheel.leftWheel);
+            doTires(wheel.rightWheel);
+        }
+    }
+
+    void doTires(WheelCollider wheel)
+    {
+        if (wheel.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform tire = wheel.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+
+        wheel.GetWorldPose(out position, out rotation); 
+
+        tire.transform.position = position;
+        tire.transform.rotation = rotation;
+    }
+}
+
+[System.Serializable]
+public class WheelElements
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+
+    public bool addWheelTorque;
+    public bool shouldSteer;
+}
